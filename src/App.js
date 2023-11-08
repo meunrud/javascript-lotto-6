061de0ee1c5fefa34b3841be1ff2d0407a16d56c
meunrud <113085963+meunrud@users.noreply.github.com>
2023-11-08 05:23:38 +0000

    테스트 통과

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import InputView from './InputView.js';
 import Machine from './LottoMachine.js';
 import LottoGenerator from './LottoGenerator.js';
 import Lotto from './Lotto.js';
-import LottoResultCalculator from './Output.js';
+import LottoResultCalculator from "./Output.js";
 
 class App {
   async play() {
     const inputview = new InputView();
     const machine = new Machine();
     const lottoGerner = new LottoGenerator();
-    await inputview.inputAmountOfMoney();
+
+    while(true){
+    try {
+     await inputview.inputAmountOfMoney();
+      break;
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
 
     const amountLotto = machine.CalculatorOfLottoAmount(inputview.playerMoney);
     const purchase = lottoGerner.purchaseLotto(amountLotto);
-
-    const inputNumber = await inputview.inputWinningNumber();
-    const winningNumber = inputview.stringToNumberArray(inputNumber);
+let inputLottoNumber = 0;
+    while(true){
+      try {
+        inputLottoNumber =  await inputview.inputWinningNumber();
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    
+    const winningNumber = inputview.stringToNumberArray(inputLottoNumber);
     const lotto = new Lotto(winningNumber);
-    const bonusNumber = await inputview.inputBonusNumber();
+    while(true){
+      try {
+         await inputview.inputBonusNumber();
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+   
 
-    const results = LottoResultCalculator.calculateResults(purchase, winningNumber, bonusNumber);
+    const results = LottoResultCalculator.calculateResults(purchase, winningNumber, inputview.bonusNumber);
     Console.print('당첨 통계\n---');
     for (const resultKey in results) {
       const result = results[resultKey];
